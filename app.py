bff2caedc01216b3cabb909e401b25531e78a20e
fatimashehab99 <fatima_shehab_sh@outlook.com>
2024-06-16 23:28:32 +0300

    packages installation

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+from google.cloud import storage
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/collect')
+def collectData():
+    url = request.get_json()
+    return url["url"]
 
 
 if __name__ == '__main__':
